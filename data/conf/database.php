8304a56e37d1f61ff42a7efd093eafe362f91fd7
bigfoot <bigfootzq@163.com>
2017-12-30 01:57:16 +0800

    添加数据库配置文件

---
+++ data/conf/database.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 配置文件
+ */
+
+return [
+    // 数据库类型
+    'type'     => 'mysql',
+    // 服务器地址
+    'hostname' => '127.0.0.1',
+    // 数据库名
+    'database' => 'lty',
+    // 用户名
+    'username' => 'root',
+    // 密码
+    'password' => 'kof3306',
+    // 端口
+    'hostport' => '3306',
+    // 数据库编码默认采用utf8
+    'charset'  => 'utf8',
+    // 数据库表前缀
+    'prefix'   => 'lty_',
+    "authcode" => '6i0Xs4fIdxTmVUtxl7',
+    //#COOKIE_PREFIX#
+];
