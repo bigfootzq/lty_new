2e90b71e54c316c97ec0e7617f449648788fffb9
bigfoot <bigfootzq@163.com>
2018-01-26 17:01:38 +0800

    修改了api/index.php，为了实现跨域请求，添加了部分header

--- public/api/index.php
+++ public/api/index.php
@@ -37,5 +37,17 @@ define('RUNTIME_PATH',__DIR__.'/../../data/runtime/api/');
 // 加载框架基础文件
 require __DIR__ . '/../../simplewind/thinkphp/base.php';
 
+// 准许跨域请求。
+header("Access-Control-Allow-Origin: * ");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+header('Access-Control-Allow-Headers:*');  
+/**
+ * 浏览器第一次在处理复杂请求的时候会先发起OPTIONS请求。路由在处理请求的时候会导致PUT请求失败。
+ * 在检测到option请求的时候就停止继续执行
+ */
+if($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
+    exit;
+}
+
 // 执行应用
 \think\App::run()->send();
